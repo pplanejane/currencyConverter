1b90ae3ff2c144c65c3bffcab9cc605ccbd7444f
Vladyslav Roskoshnyi <theraplay22848@gmail.com>
2023-03-03 13:07:29 +0200

    Update main.js
    Fix bugs when editing second input, firt input shows wrong information.
    Add listener to selects, to show new exchange info when selects edits.

--- main.js
+++ main.js
@@ -1,81 +1,71 @@
-// настройки api
 
-let myHeaders = new Headers();
-myHeaders.append("apikey", "wfycVJWNX7GfBuxy5g53jhDamxO44ek8");
-
-let requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-
-// объект с курсами, и элементы на странице
-
-const rates = {},
-      elementUSD = document.querySelector('[data-value="USD"]'),
+const elementUSD = document.querySelector('[data-value="USD"]'),
       elementEUR = document.querySelector('[data-value="EUR"]'),
       firstInput = document.querySelector("#input1"),
       secondInput = document.querySelector("#input2"),
       firstSelect = document.querySelector("#select1"),
       secondSelect = document.querySelector("#select2");
 
-// не нашел бесплатного api с всеми курсами поэтому 9 разных запросов чтобы предусмотреть все варианты развития событий
 
-getCurrency("UAH", "USD", "UAHtoUSD", elementUSD);
-getCurrency("UAH", "EUR", "UAHtoEUR", elementEUR);
-getCurrency("UAH", "UAH", "UAHtoUAH");
-getCurrency("USD", "EUR", "USDtoEUR");
-getCurrency("USD", "UAH", "USDtoUAH");
-getCurrency("USD", "USD", "USDtoUSD");
-getCurrency("EUR", "UAH", "EURtoUAH");
-getCurrency("EUR", "USD", "EURtoUSD");
-getCurrency("EUR", "EUR", "EURtoEUR");
+      let aKey = "nHeXKpxnUg5CFZumQDYWmyM2gu2XPXEM";
 
-// сам запрос и вывод текущего курса на страницу
 
-async function getCurrency(to, from, current, element = false) {
-    const response = fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=1`, requestOptions),
-          data = await (await response).text(),
-          result = await JSON.parse(data);
-          rates[current] = result.result;
+function base() {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=UAH&from=USD&amount=1`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        elementUSD.innerHTML = (data.result).toFixed(2)
+    });
 
-          if (element !== false) {
-            element.textContent = result.result.toFixed(2);
-          }
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=UAH&from=EUR&amount=1`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        elementEUR.innerHTML = (data.result).toFixed(2)
+    });
 }
+base()
 
-// очистка всех инпутов
+firstInput.addEventListener("input", () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=${secondSelect.value}&from=${firstSelect.value}&amount=${firstInput.value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    secondInput.value = (data.result).toFixed(2)
+  })
+})
 
-function clearInputs() {
-    firstInput.value = "";
-    secondInput.value = "";
-}
+secondInput.addEventListener("input", () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=${firstSelect.value}&from=${secondSelect.value}&amount=${secondInput.value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    firstInput.value = (data.result).toFixed(2)
+  })
+})
 
-// ввод в первый инпут и вывод во второй (если значение меньше 0 очистка всех инпутов, чтобы нельзя было ввести меньше нуля)
+firstSelect.addEventListener("input", () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=${secondSelect.value}&from=${firstSelect.value}&amount=${firstInput.value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    secondInput.value = (data.result).toFixed(2)
+  })
+})
 
-firstInput.oninput = function() {
-    if (firstInput.value < 0) {
-        clearInputs();
-    }
-    // первый инпут делим на значение которое находится в объкте выше, так же смотрим на выбранные валюты
-    secondInput.value = (firstInput.value / rates[firstSelect.value + "to" + secondSelect.value]).toFixed(2);
-}
+secondSelect.addEventListener("input", () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?apikey=${aKey}&to=${firstSelect.value}&from=${secondSelect.value}&amount=${secondInput.value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    firstInput.value = (data.result).toFixed(2)
+  })
+})
 
-// ввод во второй инпут и вывод в первый (если значение меньше 0 очистка всех инпутов, чтобы нельзя было ввести меньше нуля)
-
-secondInput.oninput = function() {
-    if (secondInput.value < 0) {
-        clearInputs();
-    }
-    // первый инпут умножаем на значение которое находится в объкте выше, так же смотрим на выбранные валюты
-    firstInput.value = (firstInput.value * rates[firstSelect.value + "to" + secondSelect.value]).toFixed(2);
-}
-
-// выбор валюты и изменение инпутов исходя из выбора
-
-firstSelect.oninput = function() {
-    secondInput.value = (firstInput.value / rates[firstSelect.value + "to" + secondSelect.value]).toFixed(2);
-}
-secondSelect.oninput = function() {
-    secondInput.value = (firstInput.value / rates[firstSelect.value + "to" + secondSelect.value]).toFixed(2);
-}
